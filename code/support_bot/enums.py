d9b28354d1834c8a3583d7efb8faf1d1308b5bf9
Сергей <S-E-R-G-I-O-14@yandex.ru>
2026-01-30 17:34:13 +0500

    Add admin unban command

--- code/support_bot/enums.py
+++ code/support_bot/enums.py
@@ -4,3 +4,6 @@ import enum
 class ActionName(enum.Enum):
     new_user = 'new_user', 'New user'
     user_message = 'user_message', 'User message'
+    admin_reply = 'admin_reply', 'Admin reply'
+    admin_edit = 'admin_edit', 'Admin edit'
+    admin_delete = 'admin_delete', 'Admin delete'
